af3ffccb5796a69a5a46e39c8094f92c55049246
Sergio k <valodya.kirik@gmail.com>
2023-06-06 13:42:38 +0300

    footer has been added

---
+++ app/components/layout/footer/menu.ts
@@ -0,0 +1,24 @@
+import { IFooterItem } from '../../../typedefs/typedefs';
+
+export const menu: IFooterItem[] = [
+  {
+    iconName: 'creditcard',
+    title: 'Home',
+  },
+  {
+    iconName: 'pay-circle1',
+    title: 'Payments',
+  },
+  {
+    iconName: 'switcher',
+    title: 'Services',
+  },
+  {
+    iconName: 'message1',
+    title: 'Support',
+  },
+  {
+    iconName: 'ellipsis1',
+    title: 'More',
+  },
+]
--- app/navigation/Navigation.tsx
+++ app/navigation/Navigation.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { useAuth } from '../hooks/useAuth';
 import { Auth } from '../components/screens/auth/Auth';
 import { Services } from '../components/screens/services/Services';
@@ -9,16 +9,39 @@ import { Support } from '../components/screens/support/support';
 import { Payments } from '../components/screens/payments/Payments';
 import { Home } from '../components/screens/home/Home';
 import { Profile } from '../components/screens/profile/Profile';
+import { Footer } from '../components/layout/footer/Footer';
 
 const Stack = createStackNavigator();
 
 export const Navigation: FC = () => {
+  const [name, setName] = useState<string | undefined>(undefined);
+
   const { user } = useAuth();
+  const ref = useNavigationContainerRef();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setName(
+      ref.getCurrentRoute()?.name
+    ), 100)
+
+    return () => clearTimeout(timeout);
+  }, [])
+
+  useEffect(() => {
+    const listener = ref.addListener('state', () => setName(
+      ref.getCurrentRoute()?.name,
+    ))
+
+    return () => {
+      ref.removeListener('state', listener);
+    }
+  }, [])
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        {user ? (
+    <>
+      <NavigationContainer ref={ref}>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          {user ? (
             <>
               <Stack.Screen name='Auth' component={Auth} />
               <Stack.Screen name='Payments' component={Payments} />
@@ -29,10 +52,18 @@ export const Navigation: FC = () => {
               <Stack.Screen name='Profile' component={Profile} />
             </>
           ) : (
-          <Stack.Screen name='Auth' component={Auth} />
-        )
-      }
-      </Stack.Navigator>
-    </NavigationContainer>
+            <Stack.Screen name='Auth' component={Auth} />
+          )
+          }
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      {user && name && (
+        <Footer
+          navigate={ref.navigate}
+          currentRoute={name}
+        />
+      )}
+    </>
   );
 }
