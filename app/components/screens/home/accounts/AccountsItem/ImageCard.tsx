8d4ad479a9c1209b34452bd7b02cfbbccac33c4f
Sergio k <valodya.kirik@gmail.com>
2023-06-19 18:27:58 +0300

    modal window on accounts page has been modify

--- app/components/screens/home/accounts/AccountsItem/ImageCard.tsx
+++ app/components/screens/home/accounts/AccountsItem/ImageCard.tsx
@@ -3,13 +3,11 @@ import {
   ImageSourcePropType,
   StyleSheet,
   Text,
-  Modal,
   TouchableOpacity,
-  View
 } from 'react-native';
 import { FC, useCallback, useState } from 'react';
 import { IAccount } from '../../../../../typedefs/typedefs';
-import { useProfile } from '../../../../../hooks/useProfile';
+import { ModalWindow } from '../ModalWindow/ModalWindow';
 
 interface Props {
   account: IAccount,
@@ -18,7 +16,6 @@ interface Props {
 export const ImageCard: FC<Props> = (props) => {
   const { account } = props;
   const { name, cardNumber } = account;
-  const { name: userName } = useProfile();
   const [showModal, setShowModal] = useState(false);
 
   const imageBlack: ImageSourcePropType = require('../../../../../../assets/monobank_mc-word.png');
@@ -30,7 +27,7 @@ export const ImageCard: FC<Props> = (props) => {
 
   return (
     <>
-      <TouchableOpacity onPress={() => setShowModal(true)}>
+      <TouchableOpacity onPress={handleChangeModal}>
         <ImageBackground
           source={name === 'Mono Black' ? imageBlack : imageWhite}
           resizeMode='contain'
@@ -40,36 +37,13 @@ export const ImageCard: FC<Props> = (props) => {
         </ImageBackground>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={handleChangeModal}>
-        <Modal visible={showModal} animationType="slide">
-          <View style={styles.cardContainer}>
-            <ImageBackground
-              style={styles.cardBackground}
-              source={name === 'Mono Black' ? imageBlack : imageWhite}
-            />
-
-            <Text
-              style={[
-                styles.cardNumber,
-                { color: name === 'Mono Black' ? 'white' : 'red' },
-                { bottom: name === 'Mono Black' ? '47%' : '50%' }
-              ]}
-            >
-              {cardNumber.split(' ').join('    ')}
-            </Text>
-
-            <View style={styles.cardHolder}>
-              <Text style={{ color: name === 'Mono Black' ? 'white' : 'black' }}>
-                {userName}
-              </Text>
-
-              <Text style={{ color: name === 'Mono Black' ? 'white' : 'black' }}>
-                Expiry: 12/24
-              </Text>
-            </View>
-          </View>
-        </Modal>
-      </TouchableOpacity>
+      <ModalWindow
+        showModal={showModal}
+        imageBlack={imageBlack}
+        imageWhite={imageWhite}
+        account={account}
+        handleChangeModal={handleChangeModal}
+      />
     </>
   )
 }
@@ -89,39 +63,5 @@ const styles = StyleSheet.create({
     bottom: 5,
     left: 5,
     fontWeight: '600',
-  },
-  cardContainer: {
-    width: 430,
-    height: '100%',
-    borderRadius: 10,
-    backgroundColor: '#ffffff',
-    padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  cardBackground: {
-    position: 'relative',
-    width: '100%',
-    height: 230,
-    resizeMode: 'cover',
-    borderRadius: 10,
-  },
-  cardNumber: {
-    fontSize: 28,
-    fontWeight: '900',
-    position: 'absolute',
-    left: 50,
-    bottom: '47%'
-  },
-  cardHolder: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    position: 'absolute',
-    left: 50,
-    bottom: '40%',
-    display: 'flex',
-    width: 230,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
   },
 })
